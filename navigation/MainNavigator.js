3d0129f448337f2ac7a0aa562306709e2e5b6eeb
DariuDev <60555424+DariuDev@users.noreply.github.com>
2022-01-01 16:10:03 +0330

    Update MainNavigator.js

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
- import TabNavigator from './TabNavigatior'
+ import TabNavigator from './TabNavigator'
 import VideoPlayerScreen from '../screens/VideoPlayerScreen'
 import CategoryScreen from '../screens/CategoryScreen'
 const Stack = createStackNavigator();
@@ -9,7 +9,7 @@ const Stack = createStackNavigator();
 export default function MainNavigaor() {
   return (
      <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions = {{headerShown : false}}>
         <Stack.Screen name="TabNavigator" component={TabNavigator} />
         <Stack.Screen name="VideoPlayerScreen" component={VideoPlayerScreen} />
       </Stack.Navigator>
