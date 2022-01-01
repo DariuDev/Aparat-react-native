763d66f295b4930630b2481fa15cb330f6c907c9
DariuDev <60555424+DariuDev@users.noreply.github.com>
2022-01-01 16:13:19 +0330

    Update VideoPlayerScreen.js

--- screens/VideoPlayerScreen.js
+++ screens/VideoPlayerScreen.js
@@ -1,17 +1,44 @@
-
-
 import * as React from 'react';
-import {Text, View , ScrollView , Button} from 'react-native';
+import { Text, View, ScrollView, Button } from 'react-native';
+import { Audio, Video } from 'expo-av';
 
-class VideoPlayerScreen extends React.Component {
+export default class VideoPlayerScreen extends React.Component {
+  state = {
+    fullscreen: false,
+  };
   render() {
-    const { navigation } = this.props;
+    const { navigation, route } = this.props;
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Text>
-          id: {JSON.stringify(navigation.getParam('id', 'NO-ID'))}
-        </Text>
+      <View>
+        <Button onPress={() => navigation.goBack()} title="Back" />
+        <View
+          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+         
+          <Text style = {{fontSize : 32}}>{route.params.title}</Text>
+          
+    
+          <Video
+            ref={(r) => (this.vid = r)}
+            source={{
+              uri: route.params.link,
+            }}
+            rate={1.0}
+            volume={1.0}
+            muted={false}
+            resizeMode="cover"
+            useNativeControls
+            repeat
+            style={{ width: '100%', height: 300 }}
+            onFullscreenUpdate={(e) => {
+              this.setState({ fullscreen: true });
+              console.log(e);
+            }}
+          />
+
+          
+          <Text>{route.params.description}</Text>
+          <Text>{route.params.duration}</Text>
+        </View>
       </View>
     );
   }
