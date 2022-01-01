29a422af88a5abb7d674747a3f34f22617189596
DariuDev <60555424+DariuDev@users.noreply.github.com>
2022-01-01 16:11:20 +0330

    Update TabNavigator.js

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {Text, View , ScrollView} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 //screens
 import HomeScreen from '../screens/HomeScreen'
 import CategoryScreen from '../screens/CategoryScreen'
@@ -13,7 +14,26 @@ const homeStack = createNativeStackNavigator();
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator >
+    <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Category') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }else if (route.name === 'Favorite') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })} >
       <Tab.Screen  name = 'Home' component={HomeScreen} >
       <homeStack.Screen  
       name = 'VideoPlayer' component = {VideoPlayerScreen} />
