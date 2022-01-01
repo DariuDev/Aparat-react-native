819c1754f83f3fc34da19d8d0bd01df9a8ff805d
DariuDev <60555424+DariuDev@users.noreply.github.com>
2022-01-01 16:05:10 +0330

    Update BestVideoFlatList.js

--- components/BestVideoFlatList.js
+++ components/BestVideoFlatList.js
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar,TouchableOpacity } from 'react-native';
-
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar,TouchableOpacity ,Image} from 'react-native';
+import {json} from '../webService/BestVideoApi'
 const BestVideoFlatList = ({navigation}) => {
    const [isLoading, setLoading] = useState(true);
    const [data, setData] = useState([]);
    console.log(data);
 
    useEffect(() => {
-     fetch('https://reactnative.dev/movies.json')
-       .then((response) => response.json())
-       .then((json) => setData(json))
-       .catch((error) => console.error(error))
-       .finally(() => setLoading(false));
+     console.log(json)
+
+     setData(json);
+     console.log(data)
    }, []);
 
   return (
@@ -19,10 +18,13 @@ const BestVideoFlatList = ({navigation}) => {
       <FlatList
       showsHorizontalScrollIndicator = {false}
       horizontal = {true}
-        data={data.movies}
+        data={data}
        renderItem={({ item }) => (
 <TouchableOpacity style={styles.item}
-  onPress={() => navigation.navigate('VideoPlayerScreen',{id : item.id})}>
+  onPress={() => navigation.navigate('VideoPlayerScreen',{title : item.title ,link : item.link , description : item.description , duration : item.time})}>
+  <View style = {{flex : 1}}>
+  <Image source={{uri : item.icon }} style = {styles.imageView}/>
+  </View>
     <Text style={styles.title}>{item.id + '. ' + item.title}</Text>
   </TouchableOpacity> )}
         keyExtractor={({ id }, index) => id}
@@ -38,15 +40,22 @@ const styles = StyleSheet.create({
     margin : 5,
   },
   item: {
-    background: '#00ffff',
 flexDirection : 1,
 borderRadius : 14,
+ borderWidth:1,
 marginRight : 5,
 width : 200,
   },
   title: {
-    fontSize: 32,
+    fontSize: 20,
+    padding : 7,
   },
+  imageView : {
+width: ('100%'),
+       height: ('100%'),
+       borderTopRightRadius: 14,
+       borderTopLeftRadius: 14,
+  }
 });
 
 export default BestVideoFlatList;
