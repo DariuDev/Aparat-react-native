fd155057cb5b9908a0ede63947a446af15c92e34
DariuDev <60555424+DariuDev@users.noreply.github.com>
2022-01-02 14:47:41 +0330

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import NewVideoList from '../list/NewVideoList';
 import BestVideoList from '../list/BestVideoList';
 import SpecialVideoList from '../list/SpecialVideoList';
+import NewsFlatList from '../components/NewsFlatList'
 
  import * as React from 'react';
 import {Text, View , ScrollView} from 'react-native';
@@ -8,6 +9,7 @@ import {Text, View , ScrollView} from 'react-native';
 export default function HomeScreen({navigation}) {
   return (
     <ScrollView showsVerticalScrollIndicator = {false} >
+    <NewsFlatList navigation = {navigation }/>
     <BestVideoList navigation = {navigation} />
       <NewVideoList navigation = {navigation}/>
       <SpecialVideoList navigation = {navigation}/>
