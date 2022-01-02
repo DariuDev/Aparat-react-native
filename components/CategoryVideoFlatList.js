d9f40169569504b88b051c6c977c9dfdb0780fe2
DariuDev <60555424+DariuDev@users.noreply.github.com>
2022-01-02 14:39:39 +0330

    Update CategoryVideoFlatList.js

--- components/CategoryVideoFlatList.js
+++ components/CategoryVideoFlatList.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar,TouchableOpacity  } from 'react-native';
-
-
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar,TouchableOpacity ,Image} from 'react-native';
+import {json} from '../webService/CategoryVideoApi'
 
 
 const CategoryVideoFlatList = ({navigation}) => {
@@ -10,23 +9,25 @@ const CategoryVideoFlatList = ({navigation}) => {
    console.log(data);
 
    useEffect(() => {
-     fetch('https://reactnative.dev/movies.json')
-       .then((response) => response.json())
-       .then((json) => setData(json))
-       .catch((error) => console.error(error))
-       .finally(() => setLoading(false));
+     console.log(json)
+
+     setData(json);
+    
    }, []);
 
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
       showsHorizontalScrollIndicator = {false}
-      horizontal = {false}
-        data={data.movies}
+      
+        data={data}
        renderItem={({ item }) => (
 <TouchableOpacity style={styles.item}
-  onPress={() => navigation.navigate('VideoPlayerScreen',{id : item.title})}>
-    <Text style={styles.title}>{item.id + '. ' + item.title}</Text>
+  onPress={() => navigation.navigate('VideoPlayerScreen',{title : item.title ,link : item.link , description : item.description , duration : item.time ,view : item.view})}>
+  <View style = {{flex : 1}}>
+  <Image source={{uri : item.icon }} style = {styles.imageView}/>
+  </View>
+    <Text style={styles.title}>{ item.title}</Text>
   </TouchableOpacity> )}
         keyExtractor={({ id }, index) => id}
       />
@@ -37,19 +38,29 @@ const CategoryVideoFlatList = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex : 1,
-
+    flexDirection : 'row',
+    margin : 5,
+    justifyContent : 'center'
   },
   item: {
-    background: '#ff0000',
 flexDirection : 1,
 borderRadius : 14,
-marginTop : 5
+ borderWidth:1,
+marginTop : 5,
+width : '100%',
+height : 250,
   },
   title: {
-    fontSize: 32,
-    padding : '25%',
-    color : 'white',
+    fontSize: 15,
+    padding : 4,
+    textAlign : 'center'
   },
+  imageView : {
+width: '100%',
+       height: '100%',
+       borderTopRightRadius: 14,
+       borderTopLeftRadius: 14,
+  }
 });
 
 export default CategoryVideoFlatList;
