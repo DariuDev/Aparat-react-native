664f607156ef7d8f7c694cc50537bec29e59d57b
DariuDev <60555424+DariuDev@users.noreply.github.com>
2022-01-02 14:50:31 +0330

    Create CategoryVideoApi.js

---
+++ webService/CategoryVideoApi.js
@@ -0,0 +1,60 @@
+export const json = JSON.parse(`[{
+        "id": "1",
+        "cat_id": "1",
+        "title": "Linux",
+        "icon": "http://androidsupport.ir/pack/aparat/images/linux.png",
+        "creator": "1",
+        "description": "what is linux",
+        "link": "http://androidsupport.ir/pack/aparat/videos/Linux.mp4",
+        "view": "199",
+        "time": "2:20",
+        "special": "0"
+    },   
+     {
+        "id": "2",
+        "cat_id": "2",
+        "title": "Android JetPack",
+        "icon": "http://androidsupport.ir/pack/aparat/images/jetpack.png",
+        "creator": "2",
+        "description": "Android JetPack tools for android developers",
+        "link": "http://androidsupport.ir/pack/aparat/videos/Android-Jetpack.mp4",
+        "view": "211",
+        "time": "2:14",
+        "special": "1"
+    },
+    {
+        "id": "3",
+        "cat_id": "2",
+        "title": "Dark Mode",
+        "icon": "http://androidsupport.ir/pack/aparat/images/dark-mode.jpeg",
+        "creator": "1",
+        "description": "Dark Mode Android Theme",
+        "link": "http://androidsupport.ir/pack/aparat/videos/Dark-Mode.mp4",
+        "view": "203",
+        "time": "4:21",
+        "special": "1"
+    },
+    {
+        "id": "4",
+        "cat_id": "2",
+        "title": "Installing android studio",
+        "icon": "http://androidsupport.ir/pack/aparat/images/download.png",
+        "creator": "2",
+        "description": "Do you want to learn how to instaling android studio? see this video :)",
+        "link": "http://androidsupport.ir/pack/aparat/videos/studio-install-mac.mp4",
+        "view": "125",
+        "time": "0:23",
+        "special": "0"
+    },
+    {
+        "id": "5",
+        "cat_id": "2",
+        "title": "Android studio 3.6",
+        "icon": "http://androidsupport.ir/pack/aparat/images/download.png",
+        "creator": "2",
+        "description": "Get early access to the latest features and improvements in Android Studio.",
+        "link": "http://androidsupport.ir/pack/aparat/videos/Android-Studio-3.6.mp4",
+        "view": "208",
+        "time": "08:47",
+        "special": "1"
+    }]`)
